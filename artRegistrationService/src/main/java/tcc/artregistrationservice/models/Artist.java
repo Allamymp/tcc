3a9bfa534df4f74f2efda4f0ec6f450517926fab
allamymp <allamympereira@gmail.com>
2024-02-22 23:47:09 -0300

    refactored Artist.java

--- artRegistrationService/src/main/java/tcc/artregistrationservice/models/Artist.java
+++ artRegistrationService/src/main/java/tcc/artregistrationservice/models/Artist.java
@@ -5,32 +5,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "TB_ARTIST")
 @NoArgsConstructor
 @Getter
+@Setter
 public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Setter
     private String name;
-    @Setter
-    private Date birth;
-    @Setter
+    private LocalDate birth;
     private String description;
-    @Setter
     private String artSchool;
-    @Setter
     private String country;
     @OneToMany(mappedBy = "artist")
     private List<ArtWork> artWorkList;
 
-    public Artist(String name, Date birth, String description, String artSchool
+    public Artist(String name, LocalDate birth, String description, String artSchool
             , String country) {
         this.name = name;
         this.birth = birth;
